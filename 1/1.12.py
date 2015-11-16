42649c9d6acc5e6b0c31ef277049ba41431e7d43
Shawn <gloomyshawn@gmail.com>
2015-11-16 16:22:48 +0800

    add python exsercise

--- 1/1.12.py
+++ 1/1.12.py
@@ -1,11 +1,35 @@
 __author__ = '594829'
 # -*- coding:utf-8 -*-
 
-# 查找序列中
+# 查找序列中出现次数最多的元素
 words = [
 	'look', 'into', 'my', 'eyes', 'look', 'into', 'my', 'eyes',
 	'the', 'eyes', 'the', 'eyes', 'the', 'eyes', 'not', 'around', 'the',
 	'eyes', "don't", 'look', 'around', 'the', 'the', 'eyes', 'look', 'into',
 	'my', 'eyes', "you're", 'under'
 ]
+
+from collections import Counter
+# # 统计序列中的元素
+# word_counts = Counter(words)
+# # 元素出现次数最多的前3位
+# top_three = word_counts.most_common(3)
+# print(top_three)
+
+
+morewords = ['why', 'are', 'you', 'not', 'looking', 'in', 'my', 'eyes']
+
+a = Counter(words)
+b = Counter(morewords)
+
+print(a)
+print(b)
+
+# combine counts
+c = a + b
+print(c)
+
+# Substract counts
+s = a - b
+print(s)
 
---
+++ 1/1.13.py
@@ -0,0 +1,20 @@
+__author__ = '594829'
+# -*- coding: utf-8 -*-
+
+# 利用公共键对字典列表进行排序:itemgetter函数
+rows = [
+	{'fname': 'Brian', 'lname': 'Jones', 'uid': 1003},
+	{'fname': 'David', 'lname': 'Beazleys', 'uid': 1002},
+	{'fname': 'John', 'lname': 'Cleese', 'uid': 1001},
+	{'fname': 'Big', 'lname': 'Jones', 'uid': 1004},
+]
+
+from operator import itemgetter
+
+rows_by_fname = sorted(rows, key=itemgetter('fname'))
+rows_by_uid = sorted(rows, key=itemgetter('uid'))
+rows_by_lfname = sorted(rows, key=itemgetter('lname','fname'))
+
+print(rows_by_fname)
+print(rows_by_uid)
+print(rows_by_lfname)
